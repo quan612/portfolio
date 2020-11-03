bfd906ff29552a00b6f5fd881c0a2efc0acb214e
quan612 <quan612@yahoo.com>
2020-11-02 20:55:09 -0500

    fix link to open in new tab

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -104,6 +104,23 @@ const About = () => {
           </div>
         </div>
       </div>
+      {/* <div className="social-links">
+        {networks &&
+          networks.map((network) => {
+            const { id, name, url } = network;
+            return (
+              <a
+                key={id}
+                href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                rel="noopener noreferrer"
+                target="_blank"
+                aria-label={name}
+              >
+                <i className={`fa fa-${name || 'refresh'} `} />
+              </a>
+            );
+          })}
+      </div> */}
     </div>
   );
 };
